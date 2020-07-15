63b96dc8c40e06b851bd9d60e43c3b752afe82d7
Raboija <raboijaofficial@gmail.com>
2020-07-15 12:20:21 +0100

    padding changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ReposList from './components/ReposList';
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{margin: "15px auto"}}>
+      <div className="App" style={{margin: "20px auto", padding: "20px auto"}}>
         <ReposList />
       </div>
     );
